62d58575bd153527e440b2f9bc8a7317bf5f8dd9
Asroyx <mystoganza@hotmail.com>
2023-11-21 23:47:01 +0300

    Initialize async function 'initPokemon' for retrieving and processing pokemon data

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
 const searchInput = document.querySelector("#poke-input");
 const searchBtn = document.querySelector(".btn-search");
 const pokeContainer = document.querySelector(".poke-container");
+
+const pokeCount = 151;
+
+const initPokemon = async () => {
+  for (let i = 0; i <= pokeCount; i++) {
+    
+  }
+};
