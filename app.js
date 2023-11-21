692993bb4d5b0d64c95d12105ad899e6c5624da0
Asroyx <mystoganza@hotmail.com>
2023-11-21 23:55:33 +0300

    Implement asynchronous pokemon fetching function and add in initPokemon function

--- app.js
+++ app.js
@@ -2,10 +2,18 @@ const searchInput = document.querySelector("#poke-input");
 const searchBtn = document.querySelector(".btn-search");
 const pokeContainer = document.querySelector(".poke-container");
 
-const pokeCount = 151;
+const pokeCount = 10;
 
 const initPokemon = async () => {
-  for (let i = 0; i <= pokeCount; i++) {
-    
+  for (let i = 1; i <= pokeCount; i++) {
+    await getPokemon(i);
   }
 };
+
+const getPokemon = async (id) => {
+  let url = `https://pokeapi.co/api/v2/pokemon/${id}`;
+  let res = await fetch(url);
+  let data = await res.json();
+};
+
+initPokemon();
