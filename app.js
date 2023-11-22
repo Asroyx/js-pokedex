d6a4719c81429900971df0749d0380736d038bdb
Asroyx <mystoganza@hotmail.com>
2023-11-22 23:15:57 +0300

    Add colors variable for pokemonBox background color

--- app.js
+++ app.js
@@ -2,6 +2,24 @@ const searchInput = document.querySelector("#poke-input");
 const searchBtn = document.querySelector(".btn-search");
 const pokeContainer = document.querySelector(".poke-container");
 
+const colors = {
+  fire: "#FDDFDF",
+  grass: "#DEFDEO",
+  electric: "#FCF7DE",
+  water: "#DEF3FD",
+  ground: "#f4e7da",
+  rock: "#d5d5d4",
+  fairy: "#fceaff",
+  poison: "#d6b3ff",
+  bug: "#f8d5a3",
+  dragon: "#97b3e6",
+  psychic: "#eaeda1",
+  flying: "#F5F5F5",
+  fighting: "#E6E0D4",
+  normal: "#F5F5F5",
+  ice: "#e0f5ff",
+};
+
 const pokeCount = 10;
 
 const initPokemon = async () => {
